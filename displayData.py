64a0117e03878ce0c3f6763d5fdb132e7c023055
javin7 <javinliu09@gmail.com>
2024-11-07 17:35:51 -0500

    Display SMA50, EMA20, and Closing Price

--- displayData.py
+++ displayData.py
@@ -1,16 +1,19 @@
-import fetchData
+import fetchData as data
 import matplotlib.pyplot as plt
 
-fetchData.calculateTechnicalIndicators()
-
-plt.figure(figsize=(14, 8))
-plt.plot(fetchData.stockData["Close"], label="Close RPice")
-plt.plot(fetchData.stockData["SMA365"], label="Annual SMA")
-plt.plot(fetchData.stockData["EMA365"], label="Annual EMA")
-plt.plot(fetchData.stockData["RSI"], label="Annual RSI")
 
 
-plt.title(f"{fetchData.ticker} Stock Price with SMA and EMA")
+data.calculateTechnicalIndicators()
+
+plt.figure(figsize=(14, 4))
+
+# Plot Closing Price and Movign Averages
+plt.plot(data.stockData["Close"], label="Close RPice")
+plt.plot(data.stockData["SMA50"], label="SMA")
+plt.plot(data.stockData["EMA20"], label="EMA")
+
+
+plt.title(f"{data.ticker} Stock Price with SMA and EMA")
 plt.xlabel("Date")
 plt.ylabel("Price")
 plt.legend()
--- fetchData.py
+++ fetchData.py
@@ -2,17 +2,16 @@
 import yfinance as yf
 import pandas as pd
 
-
 # Fetch data
 ticker = yf.Ticker(input("Ticker: "))
 stockData = ticker.history(period="1y")
 
-def calculateRSI(data, window):
+def calculateRSI(data, wind):
     delta = data.diff(1)
     gain = delta.where(delta > 0, 0)
     loss = -delta.where(delta < 0, 0)
-    avgGain = gain.rolling(window=window).mean()
-    avgLoss = loss.rolling(window=window).mean()
+    avgGain = gain.rolling(window=wind).mean()
+    avgLoss = loss.rolling(window=wind).mean()
     rs = avgGain / avgLoss
     rsi = 100 - (100 / (1 + rs))
     return rsi
@@ -20,13 +19,13 @@ def calculateRSI(data, window):
 # Technical Indicators
 def calculateTechnicalIndicators():
     # Simple Moving Average (SMA)
-    stockData["SMA365"] = stockData["Close"].rolling(window=20).mean()
+    stockData["SMA50"] = stockData["Close"].rolling(window=20).mean()
 
     # Exponential Moving Average (EMA)
-    stockData["EMA365"] = stockData["Close"].ewm(span=365,adjust=False).mean()
+    stockData["EMA20"] = stockData["Close"].ewm(span=20,adjust=False).mean()
 
     # Relative Strength Index (RSI)
-    stockData["RSI"] = calculateRSI(stockData["Close"], 20)
+    stockData["RSI14"] = calculateRSI(stockData["Close"], 14)
 
 
 
