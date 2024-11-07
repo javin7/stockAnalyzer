58782aa32879ed1011ebb46e6e6203d5e0383dd2
javin7 <javinliu09@gmail.com>
2024-11-07 17:35:58 -0500

    Typo

--- displayData.py
+++ displayData.py
@@ -8,7 +8,7 @@ data.calculateTechnicalIndicators()
 plt.figure(figsize=(14, 4))
 
 # Plot Closing Price and Movign Averages
-plt.plot(data.stockData["Close"], label="Close RPice")
+plt.plot(data.stockData["Close"], label="Close Price")
 plt.plot(data.stockData["SMA50"], label="SMA")
 plt.plot(data.stockData["EMA20"], label="EMA")
 
