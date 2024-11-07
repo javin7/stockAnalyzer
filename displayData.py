4f2490c36830326cf80b0a78b8da07be64adf720
javin7 <javinliu09@gmail.com>
2024-11-07 17:41:22 -0500

    Typo and cleanup

--- displayData.py
+++ displayData.py
@@ -1,18 +1,18 @@
 import fetchData as data
 import matplotlib.pyplot as plt
 
-
-
+# Calculate technical indicaotrs
 data.calculateTechnicalIndicators()
 
+# Create plot size
 plt.figure(figsize=(14, 4))
 
-# Plot Closing Price and Movign Averages
+# Plot closing price and moving averages
 plt.plot(data.stockData["Close"], label="Close Price")
 plt.plot(data.stockData["SMA50"], label="SMA")
 plt.plot(data.stockData["EMA20"], label="EMA")
 
-
+# Create data plot
 plt.title(f"{data.ticker} Stock Price with SMA and EMA")
 plt.xlabel("Date")
 plt.ylabel("Price")
