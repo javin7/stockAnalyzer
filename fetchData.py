c15a23c5fd28aec6b04c12fb93cf861e7a2798ae
javin7 <javinliu09@gmail.com>
2024-11-07 17:41:06 -0500

    Create constant for moving averages window

--- fetchData.py
+++ fetchData.py
@@ -2,10 +2,14 @@
 import yfinance as yf
 import pandas as pd
 
+# Constants
+MOVING_AVERAGE_WINDOW = 20
+
 # Fetch data
 ticker = yf.Ticker(input("Ticker: "))
-stockData = ticker.history(period="1y")
+stockData = ticker.history(period="2y")
 
+# Calculate RSI (Relative Strength Index)
 def calculateRSI(data, wind):
     delta = data.diff(1)
     gain = delta.where(delta > 0, 0)
@@ -19,10 +23,10 @@ def calculateRSI(data, wind):
 # Technical Indicators
 def calculateTechnicalIndicators():
     # Simple Moving Average (SMA)
-    stockData["SMA50"] = stockData["Close"].rolling(window=20).mean()
+    stockData["SMA50"] = stockData["Close"].rolling(window=MOVING_AVERAGE_WINDOW).mean()
 
     # Exponential Moving Average (EMA)
-    stockData["EMA20"] = stockData["Close"].ewm(span=20,adjust=False).mean()
+    stockData["EMA20"] = stockData["Close"].ewm(span=MOVING_AVERAGE_WINDOW,adjust=False).mean()
 
     # Relative Strength Index (RSI)
     stockData["RSI14"] = calculateRSI(stockData["Close"], 14)
